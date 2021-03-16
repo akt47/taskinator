d784c5635a55fdf5d2c2f24c24a5d94341fa180c
akt47 <78057009+akt47@users.noreply.github.com>
2021-03-16 10:11:51 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,6 @@ var createTaskHandler = function (){
     listItemEl.className="task-item";
     listItemEl.textContent="This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-    console.log(event);
 }
 // type of event we'll listen for and the event response to execute when the even is triggered. 
 //create a new task, styled the task item, add the text, append this element to task list. These 4 steps are key. 
