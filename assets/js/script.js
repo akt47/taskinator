e63507af5af0e9f2ad5cd2d33df84c76b58c2295
akt47 <78057009+akt47@users.noreply.github.com>
2021-03-16 13:10:26 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,24 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //important to place this before the eventlistener
 
-var createTaskHandler = function (event){
+var taskFormHandler = function (event){
     
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput= document.querySelector("select[name='task-type']").value;
   
+    //package up data as an object 
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    
+    //send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+};
+
+    var createTaskEl = function(taskDataObj) {
+        
     //create list item
     var listItemEl = document. createElement("li");
     listItemEl.className="task-item";
@@ -20,13 +32,15 @@ var createTaskHandler = function (event){
     taskInfoEl.className="task-info";
     
     //add HTML content to div
-    taskInfoEl.innerHTML= "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML= "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
     
     //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-}
+    };
+
+
 // type of event we'll listen for and the event response to execute when the even is triggered. 
 //create a new task, styled the task item, add the text, append this element to task list. These 4 steps are key. 
-formEl.addEventListener("submit", createTaskHandler); 
+formEl.addEventListener("submit", taskFormHandler); 
    
