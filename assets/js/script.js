832d5dd3fb830acb4eabc368534d05d73603c531
akt47 <78057009+akt47@users.noreply.github.com>
2021-03-16 10:05:25 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var formEl = document.querySelector("#task-form");
+
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //important to place this before the eventlistener
 
 var createTaskHandler = function (){
+    
+    event.preventDefault();
+    
     var listItemEl = document. createElement("li");
     listItemEl.className="task-item";
     listItemEl.textContent="This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
+    console.log(event);
 }
 // type of event we'll listen for and the event response to execute when the even is triggered. 
 //create a new task, styled the task item, add the text, append this element to task list. These 4 steps are key. 
-buttonEl.addEventListener("click", createTaskHandler); 
+formEl.addEventListener("submit", createTaskHandler); 
